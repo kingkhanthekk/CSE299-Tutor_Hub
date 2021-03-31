7970454ff2925c59a14e4c87edb990b0be5d5488
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2021-03-31 11:13:21 +0600

    Update settings.py

--- tutor_hub/tutor_hub/settings.py
+++ tutor_hub/tutor_hub/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tutorhub',
 ]
 
 MIDDLEWARE = [
@@ -121,5 +120,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
