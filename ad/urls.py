1d32fcd3446fd2d130275ea53310cdcd0a10b664
Arif <bm.arifuzzaman@northsouth.edu>
2021-04-28 17:01:13 +0600

    View Post In Home Page Done

--- ad/urls.py
+++ ad/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.createPostView, name='createAd'),
+    path('findTutor/', views.student_Ad, name='findTutor'),
+    path('findStudent/', views.tutor_Ad, name='findStudent'),
     path('home/', views.home, name='home'),
+    path('myAd/', views.myAd, name='myad')
 ]
--- ad/views.py
+++ ad/views.py
@@ -2,28 +2,50 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
-from .models import Ad_Student
-from .forms import Ad_Student_Form
+from .models import Ad_Student, Ad_Tutor
+from .forms import Ad_Student_Form, Ad_Tutor_Form
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
 from home.models import Student,Tutor
 
 
 @login_required
-def createPostView(request):
+def student_Ad(request):
     User = get_user_model()
     users = User.objects.all()
     if request.method == 'GET':
-        return render(request, 'ad/post_ad.html', {'form': Ad_Student_Form() })
+        return render(request, 'ad/student_ad.html', {'form': Ad_Student_Form() })
     else:
         try: 
             form = Ad_Student_Form(request.POST)
             form.save()
             return redirect('home')
         except ValueError:
-            return render(request, 'ad/post_ad.html', {'form': Ad_Student_Form(), 'error': 'Limit is Crossed' })
+            return render(request, 'ad/student_ad.html', {'form': Ad_Student_Form(), 'error': 'Limit is Crossed' })
+
+
+@login_required
+def tutor_Ad(request):
+    User = get_user_model()
+    users = User.objects.all()
+    if request.method == 'GET':
+        return render(request, 'ad/tutor_ad.html', {'form': Ad_Tutor_Form() })
+    else:
+        try: 
+            form = Ad_Tutor_Form(request.POST)
+            form.save()
+            return redirect('home')
+        except ValueError:
+            return render(request, 'ad/tutor_ad.html', {'form': Ad_Tutor_Form(), 'error': 'Limit is Crossed' })
 
 @login_required
 def home(request):
-    ads = Ad_Student.objects.order_by('-ad_created')
-    return render(request, 'ad/home.html', {'ads': ads})
+    studentAds = Ad_Student.objects.order_by('-ad_created')
+    tutorAds = Ad_Tutor.objects.order_by('-ad_created')
+    return render(request, 'ad/home.html', {'studentAds': studentAds, 'tutorAds': tutorAds})
+
+@login_required
+def myAd(request):
+    myStudentAds = Ad_Student.objects.filter(user=request.user).order_by('-ad_created')
+    myTutorAds = Ad_Tutor.objects.filter(user=request.user).order_by('-ad_created')
+    return render(request, 'ad/myad.html', {'myStudentAds': myStudentAds, 'myTutorAds': myTutorAds})
