6e6ec46214cba67c3c5ca87373745dacae7cc1f5
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2021-05-14 22:40:07 +0600

    create class form added

---
+++ tutor_hub/courses/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User, Group
+from django import forms
+from django.forms import ModelForm
+from home.models import Student, Tutor, ReviewAndComment
+from courses.models import Class, Lecture
+
+
+class CreateClassForm(ModelForm):
+    """
+    This is a conceptual class representation of Create Class Form for tutor to be used in template.
+    :param ModelForm: It inherits built-in functionalities of django ModelForm, which handels all validations
+    :type ModelForm: ModelForm
+
+    """
+    title = forms.CharField(label='Course Name', max_length=100)
+    description = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}))
+
+    class Meta:
+        model = Class
+        fields = ['title', 'description']
