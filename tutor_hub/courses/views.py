2f9893c3f78f57c4fc0d877a36757cf4acac811c
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2021-05-14 22:42:20 +0600

    join class views added

--- tutor_hub/courses/views.py
+++ tutor_hub/courses/views.py
@@ -5,5 +5,31 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import Group
+from courses.forms import CreateClassForm, CreateLectureForm, ReviewAndCommentForm
+from courses.models import Class, Lecture
+from home.models import Tutor, Student, ReviewAndComment, User
 from django.views.generic import TemplateView, DetailView, ListView, FormView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+
+def join_class(request):
+    '''
+    This will redirect the url to the join_class page, where a logged in student can join a new class and after successful class join student will be redirected to student dashboard
+
+    :param request: Takes the request to show join_class.html
+    :type request: HttpResponse
+    :return: returns arequest for a html page with form data as dictonary format
+    :rtype: render request,html page,dictonary
+
+    '''
+    if request.method == 'POST':
+        class_code = request.POST.get('class_code')
+        try:
+            myClass = Class.objects.get(class_code=class_code)
+            student = request.user.student
+            myClass.students.add(student)
+            return redirect('student_dashboard')
+        except Class.DoesNotExist:
+            messages.info(request, "Invalid Class Code")
+    context = {}
+    return render(request, 'courses/join_class.html', context)
