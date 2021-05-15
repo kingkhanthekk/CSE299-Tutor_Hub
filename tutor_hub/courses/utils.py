b9aed6ce11c5914993588ab48a58162197c45034
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2021-05-15 18:59:42 +0600

    unique slug generator added

--- tutor_hub/courses/utils.py
+++ tutor_hub/courses/utils.py
@@ -35,3 +35,32 @@ def unique_course_code_generator(instance):
     if qs_exists:
         return unique_slug_generator(instance)
     return new_class_code
+
+
+def unique_slug_generator(instance, new_slug=None):
+    """
+    This is for a Django project and it assumes your instance 
+    has a model with a slug field and a title character (char) field.
+
+    :param instance: Takes a instance of a `Class` model's object
+    :type instance: instance
+    :param new_slug: Takes a a None type argument
+    :type new_slug: None
+    :return: it returns a random string of 10 character
+    "rtype: string
+
+    """
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.title)
+
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(slug=slug).exists()
+    if qs_exists:
+        new_slug = "{slug}-{randstr}".format(
+            slug=slug,
+            randstr=random_string_generator(size=4)
+        )
+        return unique_slug_generator(instance, new_slug=new_slug)
+    return slug
