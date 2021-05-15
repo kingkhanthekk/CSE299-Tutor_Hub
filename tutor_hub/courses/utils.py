4fe38b0c7ef772e5de8f72d8744c6725620c3fda
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2021-05-15 11:24:18 +0600

    unique_course code generator added

--- tutor_hub/courses/utils.py
+++ tutor_hub/courses/utils.py
@@ -15,3 +15,23 @@ def random_string_generator(size=10, chars=string.ascii_lowercase + string.digit
 
     """
     return ''.join(random.choice(chars) for _ in range(size))
+
+
+def unique_course_code_generator(instance):
+    """
+    This is for a Django project and it assumes your instance 
+    has a model with a class_code field.
+
+    :param instance: Takes a instance of a `Class` model's object
+    :type instance: instance
+    :return: it returns a random string of 10 character
+    "rtype: string
+
+    """
+    new_class_code = random_string_generator()
+
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(class_code=new_class_code).exists()
+    if qs_exists:
+        return unique_slug_generator(instance)
+    return new_class_code
