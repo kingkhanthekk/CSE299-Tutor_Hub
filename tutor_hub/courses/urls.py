8a164c9ed600bc2422c6deab88b62bb59d95d2ab
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2021-05-18 19:16:59 +0600

    urls added for student lecture list view

--- tutor_hub/courses/urls.py
+++ tutor_hub/courses/urls.py
@@ -6,4 +6,6 @@ from courses import views
 urlpatterns = [
     path('joinClass/', views.join_class, name='join_class'),
     path('studentDashboard/', views.student_dashboard, name='student_dashboard'),
+    path('student/class/<slug:slug>/', views.student_lecture_list_View,
+         name='student_lecture_list_view'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
